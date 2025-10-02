3258a3ca2e9943c24dcb762f231caf4498f1ae2f
vcaseta <vcaseta75@gmail.com>
2025-10-02 05:45:43 +0200

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,9 +9,14 @@ export default function Page() {
 
   return (
     <main
-      className="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center"
-      style={{ backgroundImage: "url(/fondo.png)" }}
-    >
+      className="min-h-screen bg-no-repeat bg-center bg-cover flex items-center justify-center p-4"
+      style={{
+        backgroundImage: 'url/(fondo.png)',
+        backgroundSize: '100% 100%', // <- fuerza a ocupar todo el viewport
+        backgroundRepeat: 'no-repeat',
+      }}
+      >
+
       {/* Logo grande, pegado arriba */}
       <header className="w-full flex justify-center pt-10 sm:pt-14">
         <Image
