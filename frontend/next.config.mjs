b299113b0f19873bcaa4fa797d8ba190028b059f
vcaseta <vcaseta75@gmail.com>
2025-10-02 18:52:48 +0200

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // No uses "export"; así evitamos el static export y el error de revalidate
+  output: "standalone",
   reactStrictMode: true,
-  output: 'standalone', // ✅ NO usar 'export'
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
