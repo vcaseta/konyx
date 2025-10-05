dd7bad55db58b5fe7287583090e248dc7b320985
vcaseta <vcaseta75@gmail.com>
2025-10-05 09:46:35 +0200

    Update Login.tsx

--- frontend/app/components/Login.tsx
+++ frontend/app/components/Login.tsx
@@ -12,7 +12,7 @@ export default function Login({ onOk }: LoginProps) {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENPLURAL}/auth/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ password }),
