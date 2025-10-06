d164c5b36918d334af2a167ce6d09beea16c4851
vcaseta <vcaseta75@gmail.com>
2025-10-06 06:05:23 +0200

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,11 @@
-import { AuthProvider } from "../context/authContext";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body>
         <AuthProvider>
-          {children}
+          <div className="min-h-screen bg-no-repeat bg-center bg-cover" style={{ backgroundImage: "url(/fondo.png)", backgroundSize: "100% 100%" }}>
+            {children}
+          </div>
         </AuthProvider>
       </body>
     </html>
