9ec696e9660dfb5abe8c99e92f4c9a7149a5e097
vcaseta <vcaseta75@gmail.com>
2025-10-07 22:03:39 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    // Alias '@' apunta a la carpeta 'frontend'
+    config.resolve.alias['@'] = path.resolve(__dirname, 'frontend');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
